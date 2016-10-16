b717686832dc22f43f41197f533ab4a095a053a5
Esme Taljaard <esmetaljaard@esmes-macbook-pro.local>
2016-10-16 18:36:39 +0100

    Swift 3 changes

--- Queen Clips - The drag race sound board/ViewController.swift
+++ Queen Clips - The drag race sound board/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let soundFilenames = ["01", "02", "03", "04", "05", "06", "07", "08"]
+    var audioPlayers = [AVAudioPlayer]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+        
+        for sound in soundFilenames {
+            
+            do{
+                let url = URL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: "mp3")!)
+                let audioPlayer = try AVAudioPlayer(contentsOf: url)
+                
+                audioPlayers.append(audioPlayer)
+                
+            }
+                
+            catch {
+                self.audioPlayers.append(AVAudioPlayer())
+                
+            }
+        }
     }
 
     
@@ -24,6 +46,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    
+    let audioPlayer = audioPlayers[sender.tag]
+    audioPlayer.play()
+    }
 }
 
