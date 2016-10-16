e3ab3bec470850ed7127cabbf213f0f539554636
Esme Taljaard <esmetaljaard@esmes-macbook-pro.local>
2016-10-16 15:27:44 +0100

    Initial Commit

---
+++ Queen Clips - The drag race sound board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Queen Clips - The drag race sound board
+//
+//  Created by Esme Taljaard on 16/10/2016.
+//  Copyright © 2016 Esme Taljaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
