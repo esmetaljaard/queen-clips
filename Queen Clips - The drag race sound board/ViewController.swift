7a0e9cc986529620a81a1a0e08b980461886b472
Esme Taljaard <esmetaljaard@esmes-macbook-pro.local>
2016-10-16 19:13:38 +0100

    UP TO BAM!

--- Queen Clips - The drag race sound board/ViewController.swift
+++ Queen Clips - The drag race sound board/ViewController.swift
@@ -11,7 +11,7 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    let soundFilenames = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"]
+    let soundFilenames = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
     var audioPlayers = [AVAudioPlayer]()
     
 
